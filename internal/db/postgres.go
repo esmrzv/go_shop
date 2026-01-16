5a6bfb4574aa19ce1dec631a5ee8d271d962371b
esmrzv <160006249+esmrzv@users.noreply.github.com>
2026-01-16 19:37:14 +0300

    натсроен постгрес

---
+++ internal/db/postgres.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+func NewPostgres(cfg Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name, cfg.SSLMode,
+	)
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+
+	return db, nil
+}
