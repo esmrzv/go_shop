b62d560f4db79bbece6eb0104c3b4b5a8140e919
esmrzv <160006249+esmrzv@users.noreply.github.com>
2026-01-16 20:30:32 +0300

    .env + config ✅
    Подключение к Postgres ✅
    
    Сервер с graceful shutdown ✅
    
    Структура готова для repositories, services, handlers ✅

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/esmrzv/go_shop/internal/config"
+	"github.com/esmrzv/go_shop/internal/db"
+)
+
+func main() {
+	cfg := config.Load()
+
+	database, err := db.NewPostgres(db.Config{
+		Host:     cfg.DB.Host,
+		Port:     cfg.DB.Port,
+		User:     cfg.DB.User,
+		Password: cfg.DB.Password,
+		Name:     cfg.DB.Name,
+		SSLMode:  cfg.DB.SSLMode,
+	})
+
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	defer database.Close()
+
+	srv := &http.Server{
+		Addr:    ":" + cfg.AppPort,
+		Handler: http.DefaultServeMux,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		log.Printf("server listening on port %s", cfg.AppPort)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("could not listen on %s: %v\n", cfg.AppPort, err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("shutting down server...")
+
+	shutdonwCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdonwCtx); err != nil {
+		log.Fatalf("server forced to shutdown: %v", err)
+	}
+	log.Println("http server shotdown seccessfully")
+
+}
